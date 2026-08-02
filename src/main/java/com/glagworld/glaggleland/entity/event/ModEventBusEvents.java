26a15a74c9c728543dcd6028b3a94a6a89d831d4
jonahlin803 <168327681+jonahlin803@users.noreply.github.com>
2026-08-01 19:46:09 -0700

    Add Glaggleland dimension

--- src/main/java/com/glagworld/glaggleland/entity/event/ModEventBusEvents.java
+++ src/main/java/com/glagworld/glaggleland/entity/event/ModEventBusEvents.java
@@ -8,6 +8,10 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 @EventBusSubscriber(modid = Glaggleland.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ModEventBusEvents {
@@ -20,4 +24,17 @@ public class ModEventBusEvents {
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.GIGGLER.get(), GigglerEntity.createMobAttributes().build());
     }
+
+    @SubscribeEvent
+    public static void registerSpawnPlacements(RegisterSpawnPlacementsEvent event) {
+        event.register(
+                ModEntities.GIGGLER.get(),
+                SpawnPlacementTypes.ON_GROUND,
+                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                Animal::checkAnimalSpawnRules,
+                RegisterSpawnPlacementsEvent.Operation.REPLACE
+        );
+    }
 }
+
+
