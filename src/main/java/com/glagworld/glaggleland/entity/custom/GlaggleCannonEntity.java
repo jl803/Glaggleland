efce6d13484d838fe0d17b18ae9c35b9007d4b9b
jonahlin803 <168327681+jonahlin803@users.noreply.github.com>
2026-08-30 23:25:16 -0700

    Experiment with velocity cannon launch

--- src/main/java/com/glagworld/glaggleland/entity/custom/GlaggleCannonEntity.java
+++ src/main/java/com/glagworld/glaggleland/entity/custom/GlaggleCannonEntity.java
@@ -1,13 +1,13 @@
 package com.glagworld.glaggleland.entity.custom;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MoverType;
 
+import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
@@ -29,6 +29,8 @@ public class GlaggleCannonEntity extends Mob {
 
     private boolean waitingToLaunch = false;
     private boolean waitingToCheck = false;
+    private ArmorStand launchCarrier = null;
+    private int carrierLaunchTicks = 0;
 
     public GlaggleCannonEntity(EntityType<?> entityType, Level level) {
         super((EntityType<? extends Mob>) entityType, level);
@@ -53,13 +55,9 @@ public class GlaggleCannonEntity extends Mob {
             timer--;
         }
 
-        if (waitingToCheck == true && sittingPlayer != null) {
-
-            Vec3 appliedForce = new Vec3(0.0,10000.0,1.0);
-            sittingPlayer.setDeltaMovement(sittingPlayer.getDeltaMovement().add(appliedForce));
-            sittingPlayer.hurtMarked = true;
-            sittingPlayer.hasImpulse = true;
-
+        if (waitingToCheck && launchCarrier != null && carrierLaunchTicks > 0) {
+            applyLaunchVelocity(launchCarrier);
+            carrierLaunchTicks--;
         }
 
         if (timer == 0 && waitingToLaunch) {
@@ -80,9 +78,12 @@ public class GlaggleCannonEntity extends Mob {
 
         sittingPlayer.displayClientMessage(Component.literal("Your position is:" + playerPos), false);
         if (playerPos.y < 100) {
+            clearLaunchCarrier();
             sittingPlayer = null;
             return;
         }
+
+        clearLaunchCarrier();
 
         DimensionTransition transition = new DimensionTransition(
                 getServer().getLevel(Level.NETHER),
@@ -101,14 +102,8 @@ public class GlaggleCannonEntity extends Mob {
         if (isVehicle()) {
             Entity passenger = getFirstPassenger();
 
-            Vec3 currentMotion = passenger.getDeltaMovement();
-            Vec3 appliedForce = new Vec3(0.0,10000.0,1.0);
-
             passenger.stopRiding();
-
-            passenger.setDeltaMovement(currentMotion.add(appliedForce));
-            passenger.hurtMarked = true;
-            passenger.hasImpulse = true;
+            startCarrierLaunch(passenger);
 
             sittingPlayer.displayClientMessage(Component.literal("Ohhhh I'm launching it"), true);
 
@@ -116,6 +111,48 @@ public class GlaggleCannonEntity extends Mob {
             timer = 60;
 
         }
+    }
+
+    private void startCarrierLaunch(Entity passenger) {
+        ArmorStand carrier = new ArmorStand(EntityType.ARMOR_STAND, this.level());
+
+        carrier.moveTo(passenger.getX(), passenger.getY(), passenger.getZ(), passenger.getYRot(), passenger.getXRot());
+        carrier.setInvisible(true);
+        carrier.setNoGravity(true);
+        carrier.setInvulnerable(true);
+        carrier.setSilent(true);
+
+        this.level().addFreshEntity(carrier);
+        boolean startedRiding = passenger.startRiding(carrier, true);
+
+        if (!startedRiding) {
+            carrier.discard();
+            return;
+        }
+
+        launchCarrier = carrier;
+        carrierLaunchTicks = 120;
+        applyLaunchVelocity(carrier);
+    }
+
+    private void applyLaunchVelocity(Entity entity) {
+        Vec3 forward = Vec3.directionFromRotation(0.0F, this.getYRot()).normalize().scale(4.0);
+        Vec3 launchVelocity = new Vec3(forward.x, 10.0, forward.z);
+
+        entity.setDeltaMovement(launchVelocity);
+        entity.move(MoverType.SELF, launchVelocity);
+        entity.fallDistance = 0.0F;
+        entity.hurtMarked = true;
+        entity.hasImpulse = true;
+    }
+
+    private void clearLaunchCarrier() {
+        if (launchCarrier != null) {
+            launchCarrier.discard();
+            launchCarrier = null;
+        }
+
+        carrierLaunchTicks = 0;
     }
 
     @Override
